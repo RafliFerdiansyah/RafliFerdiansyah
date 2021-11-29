03f2dba42a87bba4d643fc36ac088c9841f66c69
rafliferdiansyah <rafliferdiansyah880@gmail.com>
2021-11-29 07:39:51 +0700

    Initial commit

---
+++ app/src/main/java/com/rafliferdiansyah/travel/activity/MainAboutApp.java
@@ -0,0 +1,15 @@
+package com.rafliferdiansyah.travel.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.rafliferdiansyah.travel.R;
+
+public class MainAboutApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_about_app);
+    }
+}
